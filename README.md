699ee9391fdf5f980eb0eee6e4f569767dcddaa9
paul-leonard <paul.ryan.leonard@gmail.com>
2021-01-12 08:26:49 -0800

    added list of feature requirements

---
+++ README.md
@@ -0,0 +1,22 @@
+# pythonisms
+An exploration of all that is magical in Python... or at least some of it.
+
+## Lab Submission Pull Requests
+[Lab46: Pythonisms](https://github.com/paul-leonard/pythonisms/pull/1)
+
+## Release Info
+**Author**: Paul Leonard
+**Version**: 0.9.0
+
+## Overview
+TBD
+
+## Architecture
+TBD
+
+## Change Log
+**0.9.0** 12-29-2020 - Initial beta release
+**1.0.0** 12-TBD-2020 - Initial release
+
+## Credits and Collaborations
+- none to add yet
---
+++ pythonisms/pythonisms.py
@@ -0,0 +1,18 @@
+'''
+- [ ] Use iterators/generators on a custom collection to…
+- [ ] add ability to be used in a for in loop
+- [ ] add ability to be used in a list comprehension
+- [ ] add ability to convert to a list or other collection type
+
+- [ ] Create a decorator that adds behavior to a given function. - [ ] For example…
+- [ ] Calculate the time spent in the function
+- [ ] Log relevant info for debugging purposes
+- [ ] Slow down the function (throttle)
+- [ ] Convert the return value in some way
+- [ ] Validate some condition on the way in
+
+- [ ] Use dunder methods make your code more readable and elegant. - [ ] For example…
+- [ ] add ability for two custom data structure to be considered - [ ] equal
+- [ ] add ability for custom data structure to be considered - [ ] truthy/falsy
+'''
+
